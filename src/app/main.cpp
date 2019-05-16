f65191d8de2d519818b93307f55b8fd09e9cad90
Sun.Songsong <sss09217313@163.com>
2019-05-16 22:30:35 +0800

    Add font for project
    Change-Id: I175887c2a146e1b2aa38bd8518f9b1e319069a4f

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFontDatabase>
 
 int main(int argc, char **argv)
 {
@@ -8,6 +9,8 @@ int main(int argc, char **argv)
 
     MainWindow w;
     w.show();
+
+    QFontDatabase::addApplicationFont("qrc:/font/FounderRoughAndSimplified.ttf");
 
     return a.exec();
 }
